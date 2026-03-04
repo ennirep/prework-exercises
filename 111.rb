1e90723f7dc04b153220bca4dd090f4313bb1183
ennirep <perinne@gmail.com>
2026-03-04 23:07:36 +1000

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+puts 1 + 3
+puts 15 * 2
+puts 24 / 2
+puts 984 - 1
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 2 x 2
+#111.rb:15: syntax error, unexpected local variable or method, expecting end-of-input (SyntaxError)
+#puts 2 x 2
+       ^
+
+#Error because of "x"
