af3110aeec6fa0a4e6fa250d62397cf893cf8872
ennirep <perinne@gmail.com>
2026-03-05 10:46:16 +1000

    Create 124.rb

---
+++ 124.rb
@@ -0,0 +1,31 @@
+# QUESTION 1
+# Write code to store a number in a variable.
+# Then write a condition that 
+# prints -1 if the number is less than 10, 
+# prints 1 if the number is greater than 10, 
+# and prints 0 if the number is equal to 10.
+
+x = 5
+
+if x < 10
+    puts -1
+elsif x > 10
+    puts 1
+else
+    puts 0
+end
+
+
+#QUESTION 2
+# There are two mistakes with the code below. 
+# One mistake will cause the code to crash. 
+# The other mistake doesn't cause the code to crash, but it is unnecessary code. 
+# Fix both mistakes.
+
+if x < 5
+  puts 0
+elsif x < 50
+  puts 1
+else
+  puts 2
+end
