32aa1274f670595a2a1832931ca064966a776521
ennirep <perinne@gmail.com>
2026-03-05 19:47:27 +1000

    Update 133.rb

--- 133.rb
+++ 133.rb
@@ -21,22 +21,22 @@ end
 # QUESTION 2
 # Finish annotating the code below.
 # Then run the code to see if the output matches your annotation.
-number = 0             # 1
-while 2 > number       # 2 
-  if number < 1        # 
-    puts number        #  
+number = 0             # 1  
+while 2 > number       # 2 6 9
+  if number < 1        # 3 7
+    puts number        # 4 
   end
-  number = number + 1  # 
+  number = number + 1  # 5 8
 end
-puts "Done!"           # 
+puts "Done!"           # 10 
 
 # 1: number is 0
 # 2: (2 is greater than 0, continue)
-# 3: 
-# 4: 
-# 5: 
-# 6: 
-# 7: 
-# 8: 
-# 9: 
-# 10: 
+# 3: number is less than 1, continue
+# 4: Prints out 0
+# 5: number = 0 + 1 = 1
+# 6: 2 is greater than number, continue
+# 7: number is not less than one, end
+# 8: number = 1 + 1 = 2
+# 9: 2 is not greater than number, end
+# 10: Prints out "Done!"
