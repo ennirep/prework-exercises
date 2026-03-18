20e31f21e7589aee47231e9a8b14e43f09127ba3
ennirep <perinne@gmail.com>
2026-03-18 22:36:45 +1000

    Add exercise solutions

---
+++ 211.rb
@@ -0,0 +1,71 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a program that uses a variable to store a number, then prints that number plus 15.
+
+x = 2
+
+puts x + 15
+
+#Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "biba"
+
+puts word.upcase
+
+#Write a program that uses variables to store two numbers, then prints the numbers multiplied 
+
+x = 3
+y = 4
+
+puts x*y
+
+#Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "perinne"
+
+puts word.reverse
+
+#Write a program that uses a variable to store a number, then prints the number divided by 5.
+
+x = 15
+
+puts x/5
+
+#Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+x = "håfa "
+y = "adai"
+
+puts x.upcase + y.upcase
+
+#Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "biba"
+
+puts word.length
+
+#Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+x = 3.14
+
+puts x.floor
+
+#Write a program that uses a variable to store a number, then prints the number squared.
+
+x = 3
+
+puts x**2
+
+#Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "BIBA MES CHAMORU!"
+
+puts word.downcase
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve all the problems.  However, I still need to work on remembering little things - like using qutation marks for strings.
