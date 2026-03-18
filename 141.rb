ebf530a493f85b3e6ae8f4c15ca28a7b8965e6d4
ennirep <perinne@gmail.com>
2026-03-18 10:13:31 +1000

    Create 141.rb

---
+++ 141.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Finish annotating the code below.
+x = 0                                  # 1
+names = ["Saron", "Majora", "Danilo"]  # 2
+names[1] = "Bob"                       # 3
+x = names[2]                           # 4
+names[0] = x                           # 
+x = "Charlie"                          # 
+
+# 1: x is 0
+# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
+# 3: names is: ["Saron", "Bob", "Danilo"]
+# 4: x = "Danilo"
+# 5: names [0] = "Danilo", names is ["Danilo", "Bob", "Danilo"]
+# 6: x = "Charlie"
+
+# QUESTION 2
+# Find and fix the mistakes in the code below.
+items = [3, 2, 3, 2]
+pp items
