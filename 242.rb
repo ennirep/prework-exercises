6515a949aab008586404b7b32cb472dc11c982cb
ennirep <perinne@gmail.com>
2026-03-23 10:28:51 +1000

    Create 242.rb

---
+++ 242.rb
@@ -0,0 +1,24 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+obriens = ["Malachi", "Ezra", "Hinengge"]
+
+pp obriens
+
+obriens.push("Guinaiya")
+
+pp obriens
+
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+age = {"Malachi" => 14, "Ezra" => 11}
+
+pp age
+
+age["Hinengge"] = "7"
+
+pp age
