b379abf44d2edf6a5bc56bf3ec2d081cc4fb6865
ennirep <perinne@gmail.com>
2026-04-02 16:43:49 +1000

    Create 512.rb

---
+++ 512.rb
@@ -0,0 +1,14 @@
+# QUESTION 1
+# Write a method that takes in the person hash below and increases the age by 1.
+# Then run the method and print the person hash to see if the hash was changed.
+# person = { name: "Shawn", age: 25 }
+
+def age(person)
+  person[:age] += 1
+end
+
+person = { name: "Shawn", age: 25 }
+
+age(person)
+
+pp person
