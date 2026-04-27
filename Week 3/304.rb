21b19025bb43d022ab49228572396cfe17e6541c
ennirep <perinne@gmail.com>
2026-04-28 09:21:07 +1000

    Create 304.rb

---
+++ Week 3/304.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Log into your LinkedIn account. (If you don't have one, signup for an account).
+#     https://www.linkedin.com/ 
+# Follow this document to improve your LinkedIn profile strength to at least Intermediate:
+#     https://docs.google.com/document/d/1Iyggq7G5gsHsSgh-H2g88USv-kPkLkYCbUfTHHZDo7Q/edit?usp=sharing 
+# Paste the link to your LinkedIn profile below.
+
+https://www.linkedin.com/in/perinne/
