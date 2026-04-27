09d5f9fd86a1411575237752609ed31ae5a3ce61
ennirep <perinne@gmail.com>
2026-04-28 09:21:54 +1000

    Create 311.rb

---
+++ Week 3/311.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first = "Perinne"
+last = " O'Brien"
+z = first + last
+
+puts z
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first = "Perinne"
+last = "O'Brien"
+puts "#{first} #{last}"
+
+# I forgot how to do the string interpolation so I need more practice with that.
