431c45968f2d2a75ce44567076f8c354ef96a960
ennirep <perinne@gmail.com>
2026-04-28 09:24:33 +1000

    Create 351.rb

---
+++ 351.rb
@@ -0,0 +1,9 @@
+# Create an array to store 3 local restaurant names. Then print out each name on separate lines with a while loop.
+
+restaurant = ["Fuji Ichiban", "Capriciossa", "Meskla"]
+
+index = 0
+while index < restaurant.length
+  puts restaurant[index]
+  index = index + 1
+end
